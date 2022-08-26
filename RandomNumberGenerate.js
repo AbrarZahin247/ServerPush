5c2d2a907012bcc92507ec7a4b350546f72ecbf2
Abrar Zahin <Abrar.bd27@gmail.com>
2022-08-26 10:30:40 +0600

    changed

--- RandomNumberGenerate.js
+++
@@ -1,8 +0,0 @@
-module.exports=function RandomNumGen(numlength)
-{
-    let numString=""
-    for(let i=0;i<numlength;i++){
-        numString+=Math.floor(Math.random() * 10).toString();
-    }
-    return numString;        
-}
